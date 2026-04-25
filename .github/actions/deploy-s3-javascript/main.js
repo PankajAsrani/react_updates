7b03d6c5c393d8940f1c1f6c0cad1a1ba3acacca
Pankaj Asrani <pkjasrani21@gmail.com>
2026-04-25 21:44:35 +0530

    First js action

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import core from "@actions/core"
+import github from "@actions/github"
+import exec from "@actions/exec"
+
+function run(){
+    core.notice("Hello from my customer")
+}
+
+run()
